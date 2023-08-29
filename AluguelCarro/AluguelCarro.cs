76ac71b99ccd4784ee76258902b4a01a455f843c
2021101146 <2021101146@LAB2-004>
2023-08-29 19:41:16 -0400

    Adicionar arquivos de projeto.

---
+++ AluguelCarro/AluguelCarro.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConceitosBasicos
+{
+    public class AluguelCarro
+    {
+        public static double Aluguel(int diasAluguel, double kmFinal, double kmInicial)
+        {
+            return Math.Round((95 * diasAluguel) + ((kmFinal - kmInicial) * 0.35), 2);
+        }
+    }
+}
---
+++ AluguelCarro/Program.cs
@@ -0,0 +1,5 @@
+﻿using ConceitosBasicos;
+
+int diasAluguel = 5;
+double kmFinal = 12400.98, kmInicial = 12000;
+Console.WriteLine($"O valor da diária é: R$95.00 \nO valor por KM percorrido: R$0.35 \nKM inicial: {kmInicial} \nKM Final: {kmFinal} \nO valor a se pagar para a Locadora WriteLine: R${AluguelCarro.Aluguel(diasAluguel, kmFinal, kmInicial)}");
